28c8e18f716eba9dbd3995d27ab2c90c45264b65
Jesse Maretz <jesse.maretz@gmail.com>
2020-10-14 22:36:43 -0400

    working on answer button event listeners

--- script.js
+++ script.js
@@ -11,30 +11,53 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
 let questionArr = [
     {
-        question1: 'What is Walter White diagnosed with that prompts him to "break bad?"',
-        rightAns: 'Lung Cancer',
-        wrong1: 'Dementia',
-        wrong2: 'ALS/Lou Gehrigs Disease',
-        wrong3: "Parkinson's disease"
+        question: 'What is Walter White diagnosed with that prompts him to "break bad?"', answers: [
+        {answer: 'Lung Cancer', correct: true},
+        {answer: 'Dementia', correct: false},
+        {answer: 'ALS/Lou Gehrigs Disease', correct: false},
+        {answer: "Parkinson's disease", correct: false}
+        ]
     },
     {
-        question2: "What is Saul Goodman's real name?",
-        wrong1: 'Chuck McGill',
-        wrong2: 'Howard Hamlin',
-        correct: 'Jimmy McGill',
-        wrong3: 'Don Wachtell'
-    },
+        question: "What is Saul Goodman's real name?", answers: [
+        {answer: 'Chuck McGill'},
+        {answer: 'Howard Hamlin'},
+        {correct: 'Jimmy McGill'},
+        {wrong3: 'Don Wachtell'}
+        ]
+    }, 
     {
-        question3: 'What kind of car does Walt drive?',
-        wrong1: 'PT Cruiser',
-        correct: 'Pontiac Aztek',
-        wrong2: 'Ford Pinto',
-        wrong3: 'Oldsmobile Cutlass'
+        question: 'What kind of car does Walt drive?', answers: [
+        { answer: 'PT Cruiser', correct: false },
+        { answer: 'Pontiac Aztek', correct: false }, 
+        { answer: 'Ford Pinto', correct: false }, 
+        { answer: 'Oldsmobile Cutlass', correct: false }
+        ]
     }
 ]
 let score = 0
+let startBtn = document.querySelector('#startBtn')
+let qBox = document.querySelector('#qBox')
+let currentQ = document.querySelector('#question')
+let answerBtns = document.querySelectorAll('.answerBtns')
+startBtn.addEventListener('click', startFunc)
+function startFunc(){
+    console.log('peepee')
+    startBtn.classList.add('hide')
+    qBox.classList.remove('hide')
+    answerFunc()
+}
+function answerFunc(){
+    //currentQ.innerText = questionArr.question
+    console.log('stinky')
+    answerBtns.forEach(answerBtns.addEventListener("click", e =>
+        console.log('bootycakes'))
+    )
+    /*
+    for (let i = 0; i < questionArr.length; i++) {
 
-function startQuiz(){}
-function loadNextQ(){}
+    }
+    */
+}
 function chooseAns(){}
-function ansResult(){}sessionStorage
+function ansResult(){}
